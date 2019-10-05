a1d4bec85c850743fee91a7b67fcfeb090ce481b
codedrinker <1191517759@qq.com>
2019-10-05 11:12:37 +0800

    accoss_token

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-##码匠社区
-##资料
-##工具
+## 码匠社区
+## 资料
+[Bootstrap](http://v3.bootcss.com/getting-started/)
+## 工具
+[Visual Paradigm](http://www.visual-paradigm.com)
---
+++ src/main/java/life/majiang/community/community/controller/AuthorizeController.java
@@ -0,0 +1,29 @@
+package life.majiang.community.community.controller;
+
+import life.majiang.community.community.dto.AccessTokenDTO;
+import life.majiang.community.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("078bcd431ef26180176b");
+        accessTokenDTO.setClient_secret("cdcf394b8a125ab5892392bf37b999ca89737014");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+        githubProvider.getAccessToken(accessTokenDTO);
+        return "index";
+    }
+}
