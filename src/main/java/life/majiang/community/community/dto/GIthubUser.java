580484846f9e99e14dc6c34add9e1a4b44ad4c31
codedrinker <1191517759@qq.com>
2019-10-28 09:54:51 +0800

    lombok

--- src/main/java/life/majiang/community/community/dto/GIthubUser.java
+++ src/main/java/life/majiang/community/community/dto/GIthubUser.java
@@ -1,37 +1,16 @@
 package life.majiang.community.community.dto;
 
+import lombok.Data;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 import java.io.IOException;
 
+@Data
 public class GithubUser {
     private String name;
     private Long id;
     private String bio;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
+    private String avatar_url;
 }
