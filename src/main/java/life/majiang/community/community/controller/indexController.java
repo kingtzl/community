7b868b33f0030280f8c2ea7615186db0e6340c8e
codedrinker <1191517759@qq.com>
2019-10-30 23:40:04 +0800

    完成首页问题列表

--- src/main/java/life/majiang/community/community/controller/indexController.java
+++ src/main/java/life/majiang/community/community/controller/indexController.java
@@ -1,13 +1,19 @@
 package life.majiang.community.community.controller;
 
+import life.majiang.community.community.dto.QuestionDTO;
+import life.majiang.community.community.mapper.QuestionMapper;
 import life.majiang.community.community.mapper.UserMapper;
+import life.majiang.community.community.model.Question;
 import life.majiang.community.community.model.User;
+import life.majiang.community.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import life.majiang.community.community.service.QuestionService;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class indexController {
@@ -15,8 +21,12 @@ public class indexController {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private QuestionService questionService;
+
     @GetMapping("/")
-    public String index(HttpServletRequest request) {
+    public String index(HttpServletRequest request,
+                        Model model) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0)
             for (Cookie cookie : cookies) {
@@ -29,6 +39,8 @@ public class indexController {
                     break;
                 }
         }
+        List<QuestionDTO> questionList = questionService.list();
+        model.addAttribute("questions",questionList);
         return "index";
     }
 }
--- src/main/java/life/majiang/community/community/dto/GIthubUser.java
+++ src/main/java/life/majiang/community/community/dto/GIthubUser.java
@@ -1,11 +1,6 @@
 package life.majiang.community.community.dto;
 
 import lombok.Data;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
-import java.io.IOException;
 
 @Data
 public class GithubUser {
